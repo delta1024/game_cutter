e516a8f342d34f2dbd6f94482b12c417328342de
Jacob Stannix <jakestannix@gmail.com>
2024-10-04 12:09:29 -0600

    feat: events are now pairs

--- lib/game_cutter/event_bus.rb
+++ lib/game_cutter/event_bus.rb
@@ -3,15 +3,22 @@ module GameCutter
     def initialize
       @events = @callbacks.map { |k, _| [k, []] }.to_h
     end
-    def subscribe(key, sub) 
-      @events.fetch(key) << sub
+    def subscribe(*vals) 
+      vals.each_slice(2) do |key, sub|
+        @events.fetch(key) << sub
+      end
     end
-    def unsubscribe(key, sub)
-      @events[key] = @events.fetch(key).filter { |s| not s.equal? sub }
+    def unsubscribe(*vals)
+      vals.each_slice(2) do |key, sub|
+        @events[key] = @events.fetch(key).filter { |s| not s.equal? sub }
+      end
     end
-    def publish(key, val) 
-      callback = @callbacks.fetch(key)
-      @events[key].each { |o| callback.call(o, val) }
+
+    def publish(*vals) 
+      vals.each_slice(2) do |key, val|
+        callback = @callbacks.fetch(key)
+        @events[key].each { |o| callback.call(o, val) }
+      end
     end
   end
 end
